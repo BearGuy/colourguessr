64400a840baca488a3843f08a820c4f181bbaab4
Stephen Peterkins <stephenpeterkins@Stephens-MacBook-Pro.local>
2023-08-10 20:59:30 -0400

    update serviceWorker logic to update cache on new app_version

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,23 +1,50 @@
-const app_version = "colour-guessr-0.0.5"
+const app_version = "colour-guessr-0.0.6";
 const assets = [
   "/",
   "/index.html",
   "/css/style.css",
   "/js/app.js",
-]
+];
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
     caches.open(app_version).then(cache => {
-      cache.addAll(assets)
+      return cache.addAll(assets);
     })
-  )
-})
+  );
+});
+
+self.addEventListener("activate", activateEvent => {
+  activateEvent.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => cacheName !== app_version)
+          .map(cacheName => caches.delete(cacheName))
+      );
+    })
+  );
+});
 
 self.addEventListener("fetch", fetchEvent => {
   fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request)
+    caches.match(fetchEvent.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(fetchEvent.request).then(response => {
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        const clonedResponse = response.clone();
+
+        caches.open(app_version).then(cache => {
+          cache.put(fetchEvent.request, clonedResponse);
+        });
+
+        return response;
+      });
     })
-  )
-})
+  );
+});
